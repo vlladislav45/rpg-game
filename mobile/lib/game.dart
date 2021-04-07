28a8c5cf0b47e665f7bdfe04fa5462f923482597
Vladislav Enev <vlladislav45@gmail.com>
2021-04-07 14:49:06 +0300

    feat: isometric tile map

---
+++ mobile/lib/game.dart
@@ -0,0 +1,27 @@
+import 'package:flame/game.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:rpg_game/map/isometric_tile_map.dart';
+
+class MyGame extends StatefulWidget {
+  const MyGame({Key key}) : super(key: key);
+
+  @override
+  State<MyGame> createState() {
+    return MyGameState();
+  }
+}
+
+class MyGameState extends State<MyGame> {
+  final IsometricTileMap _isometricTileMap = IsometricTileMap();
+  
+  @override
+  Widget build(BuildContext context) {
+    final myGame = _isometricTileMap;
+    return MaterialApp(
+      home: Scaffold(
+        body: GameWidget<IsometricTileMap>(game: myGame),
+      ),
+    );
+  }
+}
