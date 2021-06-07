9a96f2a3628047a937d9968827f163eb53ca09ee
vladislav <vlladislav45@gmail.com>
2021-06-07 18:01:45 +0300

    style: (fe) login screen

--- mobile/lib/presentation/screens/login_screen.dart
+++ mobile/lib/presentation/screens/login_screen.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:auto_size_text/auto_size_text.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
@@ -5,6 +7,7 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:rpg_game/logic/blocs/online/online_bloc.dart';
 import 'package:rpg_game/logic/blocs/online/online_event.dart';
 import 'package:rpg_game/logic/blocs/online/online_state.dart';
+import 'package:rpg_game/utils/hex_color.dart';
 
 class LoginScreen extends StatefulWidget {
   @override
@@ -37,100 +40,131 @@ class _LoginScreenState extends State<LoginScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: <Widget>[
-          Container(
-            child: AutoSizeText(
-              'Welcome to Daily Wars',
-              minFontSize: 15,
-              maxFontSize: 25,
-              style: Theme.of(context).textTheme.headline1,
-            ),
-          ),
-
-          TextField(
-            controller: _usernameController,
-            decoration: InputDecoration(
-              border: OutlineInputBorder(),
-              labelText: 'Username',
-            ),
-          ),
-          TextField(
-            controller: _passwordController,
-            obscureText: true,
-            decoration: InputDecoration(
-              border: OutlineInputBorder(),
-              labelText: 'Password',
-            ),
-          ),
-          BlocBuilder<OnlineBloc, OnlineState>(builder: (context, state) {
-            print(state);
-            if (state is OnlineInitialState) {
-              return Container();
-            }
-            if (state is OnlineConnectingState) {
-              return CircularProgressIndicator();
-            }
-            if (state is OnlineConnectErrorState) {
-              return Column(
-                children: [
-                  Text('No internet connection, failed to connect'),
-                ],
-              );
-            }
-            if (state is OnlineConnectTimeoutState) {
-              return Column(
-                children: [
-                  Text('Connection timed out'),
-                ],
-              );
-            }
-            if (state is OnlineDisconnectedState) {
-              return Column(
-                children: [
-                  Text('Disconnected'),
-                ],
-              );
-            }
-            if (state is OnlineErrorState) {
-              return Column(
-                children: [
-                  Text('An error occurred'),
-                ],
-              );
-            }
-            return Column(
+      body: Container(
+        width: double.infinity,
+        color: Color(HexColor.convertHexColor('#31572c')),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            Row(
+              mainAxisAlignment: MainAxisAlignment.end,
               children: <Widget>[
-                Text('Connected!'),
-                ElevatedButton(
-                  onPressed: () {
-                    _onlineBloc.add(OnlineAuthenticationEvent(
-                      username: _usernameController.text.toString(),
-                      password: _passwordController.text.toString(),
-                    ));
-
-                    if(state is OnlineAuthenticatedState)
-                      Navigator.of(context).pushNamed('/game');
-                  },
+                InkWell(
                   child: Container(
-                    child: AutoSizeText(
-                      'Login',
-                      style: TextStyle(
-                        fontWeight: FontWeight.bold,
+                    width: 50,
+                    height: 50,
+                    decoration: BoxDecoration(
+                      image: DecorationImage(
+                          image: AssetImage('assets/images/login/exit.png'),
+                          fit: BoxFit.fill),
+                    ),
+                  ),
+                  onTap: () {
+                    exit(0);
+                  },
+                ),
+              ],
+            ),
+            Container(
+              child: AutoSizeText(
+                'Welcome to \n Daily Wars',
+                minFontSize: 15,
+                maxFontSize: 25,
+                style: Theme.of(context).textTheme.headline1,
+              ),
+            ),
+            Container(
+              width: MediaQuery.of(context).size.width / 2.5,
+              padding: EdgeInsets.symmetric(horizontal: 8, vertical: 16),
+              child: TextField(
+                controller: _usernameController,
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  hintText: 'Enter Your Username',
+                ),
+              ),
+            ),
+            Container(
+              width: MediaQuery.of(context).size.width / 2.5,
+              padding: EdgeInsets.symmetric(horizontal: 8, vertical: 16),
+              child: TextField(
+                controller: _passwordController,
+                obscureText: true,
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  hintText: 'Enter Your Password',
+                ),
+              ),
+            ),
+            BlocBuilder<OnlineBloc, OnlineState>(builder: (context, state) {
+              print(state);
+              if (state is OnlineInitialState) {
+                return Container();
+              }
+              if (state is OnlineConnectingState) {
+                return CircularProgressIndicator();
+              }
+              if (state is OnlineConnectErrorState) {
+                return Column(
+                  children: [
+                    Text('No internet connection, failed to connect'),
+                  ],
+                );
+              }
+              if (state is OnlineConnectTimeoutState) {
+                return Column(
+                  children: [
+                    Text('Connection timed out'),
+                  ],
+                );
+              }
+              if (state is OnlineDisconnectedState) {
+                return Column(
+                  children: [
+                    Text('Disconnected'),
+                  ],
+                );
+              }
+              if (state is OnlineErrorState) {
+                return Column(
+                  children: [
+                    Text('An error occurred'),
+                  ],
+                );
+              }
+              return Column(
+                children: <Widget>[
+                  Text('Connected!'),
+                  ElevatedButton(
+                    onPressed: () {
+                      _onlineBloc.add(OnlineAuthenticationEvent(
+                        username: _usernameController.text.toString(),
+                        password: _passwordController.text.toString(),
+                      ));
+                    },
+                    child: Container(
+                      child: AutoSizeText(
+                        'Login',
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                        ),
                       ),
                     ),
                   ),
-                ),
-                Builder(builder: (context) {
-                  if (state is OnlineAuthenticatedState) {
-                    return Text('The user is authenticated');
-                  }
-                  return Text('The user is not authenticated');
-                })
-              ],
-            );
-          }),
-        ],
+                  Builder(builder: (context) {
+                    if (state is OnlineAuthenticatedState) {
+                      Navigator.of(context).pushNamed('/game');
+                    } else {
+                      return Text('The user is not authenticated');
+                    }
+                    return Container();
+                  })
+                ],
+              );
+            }),
+          ],
+        ),
       ),
     );
   }
--- mobile/lib/utils/theme.dart
+++ mobile/lib/utils/theme.dart
@@ -2,14 +2,14 @@ import 'package:rpg_game/utils/hex_color.dart';
 import 'package:flutter/material.dart';
 
 class CustomTheme {
-  static Color _lightPrimary = Color(HexColor.convertHexColor('#d61693')); // main color
+  static Color _lightPrimary = Color(HexColor.convertHexColor('#7f6000')); // main color
   static Color _lightPrimaryVariantColor =
       Color(HexColor.convertHexColor('#d61693')); // Appbar color
-  static Color _lightSecondaryColor = Color(HexColor.convertHexColor('#5df9d1'));
+  static Color _lightSecondaryColor = Color(HexColor.convertHexColor('#000000'));
   static Color _lightSecondaryDonateColor = Color(HexColor.convertHexColor('#ffd400'));
   static Color _lightOnPrimaryColor = Color(HexColor.convertHexColor('#71cdc5')); // text
   static Color _lightAccentColor =
-      Color(HexColor.convertHexColor('#fae950')); // Title texts
+      Color(HexColor.convertHexColor('#c8ba2b')); // Title texts
 
   static final ThemeData theme = ThemeData(
     colorScheme: ColorScheme.light(
@@ -36,19 +36,19 @@ class CustomTheme {
             Shadow(
                 // bottomLeft
                 offset: Offset(-1.5, -1.5),
-                color: Color(HexColor.convertHexColor('#d134a0'))),
+                color: Color(HexColor.convertHexColor('#000000'))),
             Shadow(
                 // bottomRight
                 offset: Offset(1.5, -1.5),
-                color: Color(HexColor.convertHexColor('#d134a0'))),
+                color: Color(HexColor.convertHexColor('#000000'))),
             Shadow(
                 // topRight
                 offset: Offset(1.5, 1.5),
-                color: Color(HexColor.convertHexColor('#d134a0'))),
+                color: Color(HexColor.convertHexColor('#000000'))),
             Shadow(
                 // topLeft
                 offset: Offset(-1.5, 1.5),
-                color: Color(HexColor.convertHexColor('#d134a0'))),
+                color: Color(HexColor.convertHexColor('#000000'))),
           ]),
       headline2: TextStyle(
         fontSize: 22,
