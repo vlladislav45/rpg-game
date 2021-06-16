07d151eb98e8a7a2703e392b423df00356f412fd
Vladislav <vlladislav45@gmail.com>
2021-06-16 20:13:06 +0300

    * character status overlay

--- mobile/lib/presentation/widgets/character/character_overlay.dart
+++ mobile/lib/presentation/widgets/character/character_overlay.dart
@@ -1,9 +1,11 @@
-
 import 'package:auto_size_text/auto_size_text.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_animation_progress_bar/flutter_animation_progress_bar.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:rpg_game/game.dart';
+import 'package:rpg_game/logic/blocs/online/online_bloc.dart';
+import 'package:rpg_game/logic/blocs/online/online_state.dart';
 import 'package:rpg_game/utils/hex_color.dart';
 
 Widget characterOverlayBuilder(BuildContext context, MyGame myGame) {
@@ -17,57 +19,90 @@ class CharacterOverlay extends StatefulWidget {
   }
 }
 
-class CharacterOverlayState extends State<CharacterOverlay> with TickerProviderStateMixin {
+class CharacterOverlayState extends State<CharacterOverlay>
+    with TickerProviderStateMixin {
   AnimationController? controller;
 
   @override
   void initState() {
+    super.initState();
+
     controller = AnimationController(
       vsync: this,
       duration: const Duration(seconds: 5),
     )..addListener(() {
-      setState(() {});
-    });
+        setState(() {});
+      });
     controller!.repeat(reverse: true);
-    super.initState();
   }
 
   @override
   Widget build(BuildContext context) {
     return Container(
       padding: EdgeInsets.all(10.0),
-      color: Color(HexColor.convertHexColor('#808080')),
       alignment: Alignment.topLeft,
-      width: MediaQuery.of(context).size.width / 6,
-      height: MediaQuery.of(context).size.width / 10,
-      child: Column(
-        children: <Widget>[
-          Container(
-            width: double.infinity,
-            child: Row(
-              children: <Widget>[
-                AutoSizeText(
-                    '1lv'
-                ),
+      decoration: BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topRight,
+            end: Alignment.bottomLeft,
+            colors: [
+              Colors.black45,
+              Colors.black87,
+            ],
 
-                AutoSizeText(
-                    'My Name'
-                ),
-              ],
-            )
-          ),
-          FAProgressBar(
-            maxValue: 2000,
-            currentValue: 800,
-            displayText: 'Hp',
-          ),
-          FAProgressBar(
-            currentValue: 1564,
-            displayText: 'Mana',
-            progressColor: Colors.blue,
-          ),
-        ],
+          )
       ),
+      width: MediaQuery.of(context).size.width / 5,
+      height: MediaQuery.of(context).size.width / 12,
+      child: BlocBuilder<OnlineBloc, OnlineState>(builder: (context, state) {
+        if(state is OnlineAuthenticatedState) {
+          return Column(
+            children: <Widget>[
+              Container(
+                  width: double.infinity,
+                  child: Row(
+                    children: <Widget>[
+                      Container(
+                        margin: EdgeInsets.only(right: 10.0, bottom: 5.0),
+                        child: AutoSizeText(
+                            'Lv. ${state.userViewModel.characters[0].level}',
+                          style: TextStyle(
+                            color: Colors.white,
+                          ),
+                        ),
+                      ),
+
+                      Container(
+                        child: AutoSizeText(
+                            '${state.userViewModel.username}',
+                            style: TextStyle(
+                              color: Colors.white,
+                            ),
+                        ),
+                      ),
+
+                    ],
+                  )),
+              Container(
+                margin: EdgeInsets.only(bottom: 5.0),
+                child: FAProgressBar(
+                  maxValue: state.userViewModel.characters[0].hp,
+                  currentValue: state.userViewModel.characters[0].hp,
+                  displayText: '${state.userViewModel.characters[0].hp} / ',
+                  progressColor: Color(HexColor.convertHexColor('#A42324')),
+                ),
+              ),
+              FAProgressBar(
+                maxValue: state.userViewModel.characters[0].mana,
+                currentValue: state.userViewModel.characters[0].mana,
+                displayText: '${state.userViewModel.characters[0].mana} / ',
+                progressColor: Color(HexColor.convertHexColor('#106FB5')),
+              ),
+            ],
+          );
+        }
+        return CircularProgressIndicator();
+      }),
     );
   }
 }
